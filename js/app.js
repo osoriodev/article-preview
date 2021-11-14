848f6ce0816e7fdc75b6063a02d791e3ea66928f
David Osorio <osoriodev@outlook.com>
2021-11-13 23:23:21 -0600

    Add btn events

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+const popupMessage = document.querySelector('.card__social')
+
+const btn = document.querySelector('.card__btn')
+
+const displayPopup = () => {
+  popupMessage.classList.toggle('display-message')
+}
+
+btn.addEventListener('click', displayPopup)
